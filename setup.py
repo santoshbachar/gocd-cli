a1db120544644952b8925df7a1a5e8b56c2897ab
Santosh Kumar Bachar <santoshbachar@gmail.com>
2026-03-11 23:17:00 +0530

    chore: update Python version requirement

--- setup.py
+++ setup.py
@@ -56,10 +56,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.13.9'
+        'Programming Language :: Python :: 3.12.12'
     ],
-    python_requires='==3.13.9'
+    python_requires='==3.12.12'
 )
